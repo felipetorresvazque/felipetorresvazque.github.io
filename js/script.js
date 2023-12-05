18dc5302ef850c10b2cf6eb758d74db3f89b1c4d
felipe torres vazque <felipe.torres.vazque@gmail.com>
2023-12-05 15:52:32 -0300

    configuracion php

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.form');
+form.addEventListener('submit',function (e){
+    e.preventDefault();
+    sendMessage(form);
+})
+
+async function sendMessage(form){
+    const formdata = new formdata(form);
+    if(formdata){
+        const url = 'php/sendMessage.php'
+        const response = await fetch(url,{
+            method:"POST",
+            body: formdata
+
+        });
+        if(response.ok){
+            form.reset();
+            alert('form sent !');
+        }else{
+            alert('form error!');
+        }
+    }
+}
