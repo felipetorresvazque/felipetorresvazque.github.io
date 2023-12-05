40a4cff983d9facd2454d8b1634ac5b47213b7fa
felipe torres vazque <felipe.torres.vazque@gmail.com>
2023-12-05 16:57:24 -0300

    coreccion menores

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-const form = document.querySelector('.form');
-form.addEventListener('submit',function (e){
-    e.preventDefault();
-    sendMessage(form);
-})
+"use strict";
 
-async function sendMessage(form){
-    const formdata = new formdata(form);
-    if(formdata){
-        const url = 'php/sendMessage.php'
-        const response = await fetch(url,{
-            method:"POST",
-            body: formdata
+const form = document.querySelector(".form");
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  sendMessage(form);
+});
 
-        });
-        if(response.ok){
-            form.reset();
-            alert('form sent !');
-        }else{
-            alert('form error!');
-        }
+async function sendMessage(form) {
+  const formdata = new formdata(form);
+  if (formdata) {
+    const url = "php/sendMessage.php";
+    const response = await fetch(url, {
+      method: "POST",
+      body: formdata,
+    });
+    if (response.ok) {
+      form.reset();
+      alert("form sent !");
+    } else {
+      alert("form error!");
     }
+  }
 }
